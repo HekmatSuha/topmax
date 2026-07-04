a7be2835240b5f19b109478846700d30141d721c
HekmatSuha <hekmatsuha353@gmail.com>
2026-07-04 11:40:59 +0600

    backgroundColorChange

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1354,7 +1354,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50/50">
+    <div className="min-h-screen flex flex-col bg-slate-100">
       <Navbar
         onNavigate={handleNavigate}
         currentPage={currentPage}
